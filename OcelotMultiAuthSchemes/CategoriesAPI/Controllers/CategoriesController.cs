7e2362365d7968186b4a50bec9438bf5ac9c73a2
Ramzi Bdair <bdairrm@saptco.com.sa>
2022-11-28 16:45:56 +0300

    initial

---
+++ OcelotMultiAuthSchemes/CategoriesAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,49 @@
+﻿using CategoriesAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CategoriesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<Category>> GetCategories()
+        {
+            int size = 10;
+            var categories = new List<Category>(size);
+            
+            for (int index = 1; index <= size; index++)
+            {
+                categories.Add(new()
+                {
+                    Id = index,
+                    Description = $"Category {index}"
+                });
+            }
+
+            return categories;
+        }
+        
+        [HttpGet]
+        [Route("GetCategories2")]
+        public ActionResult<IEnumerable<Category>> GetCategories2()
+        {
+            int size = 10;
+            var categories = new List<Category>(size);
+
+            for (int index = 1; index <= size; index++)
+            {
+                categories.Add(new()
+                {
+                    Id = index,
+                    Description = $"Category {index}"
+                });
+            }
+
+            return categories;
+        }
+    }
+}
